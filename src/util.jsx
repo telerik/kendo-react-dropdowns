742931a11971726780e735b790f44564a6197046
Alexander Valchev <a.valchevbox@gmail.com>
2016-02-19 14:59:52 +0200

    feat(utils): add util methods

--- src/util.jsx
+++ src/util.jsx
@@ -1,7 +1,7 @@
-export default function caret(element, startIndex, endIndex = startIndex) {
-    let start = startIndex;
-    let end = endIndex;
-    const isPosition = start !== undefined;
+/*
+function caret(element, startIndex, endIndex = startIndex) {
+    const isPosition = startIndex !== undefined;
+    let selectionStart;
 
     if (isPosition && element.disabled) {
         return undefined;
@@ -10,11 +10,45 @@ export default function caret(element, startIndex, endIndex = startIndex) {
     if (element.selectionStart !== undefined) {
         if (isPosition) {
             element.focus();
-            element.setSelectionRange(start, end);
+            element.setSelectionRange(startIndex, endIndex);
         } else {
-            start = [ element.selectionStart, element.selectionEnd ];
+            selectionStart = element.selectionStart;
         }
     }
 
-    return start;
+    return selectionStart;
 }
+*/
+
+function caretSelection(element, startIndex, endIndex = startIndex) {
+    if (startIndex && !element.disabled && element.selectionStart !== undefined) {
+        element.focus();
+        element.setSelectionRange(startIndex, endIndex);
+    }
+}
+
+function caretPosition(element) {
+    return element.disabled ? undefined : element.selectionStart;
+}
+
+function indexOfWordAtCaret(caretIdx, text, separator) {
+    return separator ? text.substring(0, caretIdx).split(separator).length - 1 : 0;
+}
+
+function wordAtCaret(caretIdx, text, separator) {
+    return text.split(separator)[indexOfWordAtCaret(caretIdx, text, separator)];
+}
+
+function replaceWordAtCaret(caretIdx, text, word, separator) {
+    let words = text.split(separator);
+
+    words.splice(indexOfWordAtCaret(caretIdx, text, separator), 1, word);
+
+    if (separator && words[words.length - 1] !== "") {
+        words.push("");
+    }
+
+    return words.join(separator);
+}
+
+export { caretPosition, caretSelection, indexOfWordAtCaret, wordAtCaret, replaceWordAtCaret };
