8cc187028968673b2da6e0e42da4541f8df04bf1
Atanas Georgiev <atanas.georgiev@telerik.com>
2016-02-26 15:36:43 +0200

    test(autocomplete): Added new functional test - filter corresponding options

--- e2e-tests/KendoAutoComplete.js
+++ e2e-tests/KendoAutoComplete.js
@@ -1,9 +1,19 @@
 // spec.js
-describe('Protractor Demo App', function() {
-    it('should add one and two', function() {
-        browser.get('http://localhost:3000/examples/basic.html');
+describe('AutoComplete basic usage example', function() {
+    it('should type a letter and assert autofill', function() {
+        browser.get('http://localhost:8888/examples/basic.html');
         const autoComplete = element(by.tagName('input'));
         autoComplete.sendKeys('f');
         expect(autoComplete.getAttribute('value')).toEqual('foo1');
     });
+	
+	it('should filter and assert corresponding options', function() {
+        browser.get('http://localhost:8888/examples/basic.html');
+        const autoComplete = element(by.tagName('input'));
+		const optionItem = element(by.css('.k-item'));
+        autoComplete.sendKeys('dsa');
+        expect(optionItem.getText()).toEqual('dsa');
+		expect(optionItem.getText()).not.toEqual('foo6');
+    });
+	
 });
