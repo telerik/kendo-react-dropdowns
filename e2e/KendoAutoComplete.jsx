efdb9c3c70248dbfc2d395f0de64ab8b9398c675
Alexander Valchev <a.valchevbox@gmail.com>
2016-03-07 18:44:14 +0200

    test(autocomplete): add more tests

--- e2e/KendoAutoComplete.jsx
+++ e2e/KendoAutoComplete.jsx
@@ -8,64 +8,28 @@ import { type } from './util';
 // it exports `$` and `withRoot` - higher order function for describe (example below)
 import { withRoot } from 'e2e-utils';
 
+const data = [
+    { text: "Albania", value: "Alb" },
+    { text: "Andorra", value: "And" },
+    { text: "Belarus", value: "Blg" },
+    { text: "Belgium", value: "Bls" },
+    { text: "Finland", value: "Fin" },
+    { text: "France", value: "Fra" },
+    { text: "Georgia", value: "Geo" },
+    { text: "Germany", value: "Ger" },
+    { text: "Greece", value: "Gre" },
+    { text: "Hungary", value: "Hun" },
+    { text: "Iceland", value: "Ice" },
+    { text: "Ireland", value: "Ire" },
+    { text: "Italy", value: "Ita" }
+];
+
 // `root` parameter is a jQuery object which includes chai-jquery in it.
 // chai-jquery adds a should method to the jQuery object.
 // See https://github.com/chaijs/chai-jquery#assertions for available assertions.
 describe('KendoAutoComplete', withRoot(root => {
 
-    const data = [
-        { text: "Albania", value: "Alb" },
-        { text: "Andorra", value: "And" },
-        { text: "Armenia", value: "Arm" },
-        { text: "Austria", value: "Aus" },
-        { text: "Azerbaijan", value: "Aze" },
-        { text: "Belarus", value: "Blg" },
-        { text: "Belgium", value: "Bls" },
-        { text: "Bosnia & Herzegovina", value: "Her" },
-        { text: "Bulgaria", value: "Bul" },
-        { text: "Croatia", value: "Cro" },
-        { text: "Cyprus", value: "Cyp" },
-        { text: "Czech Republic", value: "Rep" },
-        { text: "Denmark", value: "Den" },
-        { text: "Estonia", value: "Est" },
-        { text: "Finland", value: "Fin" },
-        { text: "France", value: "Fra" },
-        { text: "Georgia", value: "Geo" },
-        { text: "Germany", value: "Ger" },
-        { text: "Greece", value: "Gre" },
-        { text: "Hungary", value: "Hun" },
-        { text: "Iceland", value: "Ice" },
-        { text: "Ireland", value: "Ire" },
-        { text: "Italy", value: "Ita" },
-        { text: "Kosovo", value: "Kos" },
-        { text: "Latvia", value: "Lat" },
-        { text: "Liechtenstein", value: "Lie" },
-        { text: "Lithuania", value: "Lit" },
-        { text: "Luxembourg", value: "Lux" },
-        { text: "Macedonia", value: "Mac" },
-        { text: "Malta", value: "Mal" },
-        { text: "Moldova", value: "Mol" },
-        { text: "Monaco", value: "Mon" },
-        { text: "Montenegro", value: "Mon" },
-        { text: "Netherlands", value: "Net" },
-        { text: "Norway", value: "Nor" },
-        { text: "Poland", value: "Pol" },
-        { text: "Portugal", value: "Por" },
-        { text: "Romania", value: "Rom" },
-        { text: "Russia", value: "Rus" },
-        { text: "San Marino", value: "Mar" },
-        { text: "Serbia", value: "Ser" },
-        { text: "Slovenia", value: "Slo" },
-        { text: "Spain", value: "Spa" },
-        { text: "Sweden", value: "Swe" },
-        { text: "Switzerland", value: "Swi" },
-        { text: "Turkey", value: "Tur" },
-        { text: "Ukraine", value: "Ukr" },
-        { text: "United Kingdom", value: "Kin" },
-        { text: "Vatican City", value: "VC" }
-    ];
-
-    const render = (filter) => {
+    function render(filter) {
         let dataList;
 
         if (filter) {
@@ -77,19 +41,9 @@ describe('KendoAutoComplete', withRoot(root => {
         }
 
         ReactDOM.render( <KendoAutoComplete data={dataList} {...props} />, root[0]);
-    };
+    }
 
     let props;
-
-    it('should autofill when typing', () => {
-        props = { suggest: true, valueField: "text", onSearch: render };
-        render("");
-
-        const input = root.find("input")[0];
-
-        type(input, "f");
-        expect(input.value).toEqual("Finland");
-    });
 
     it('should render the provided data', () => {
         props = { valueField: "text" };
@@ -103,6 +57,43 @@ describe('KendoAutoComplete', withRoot(root => {
         });
     });
 
+    it('should fire onSearch event when typing', () => {
+        props = {
+            onSearch: function(filter) {
+                expect(filter).toEqual("f");
+            },
+            valueField: "text"
+        };
+        render();
+
+        const input = root.find("input")[0];
+        type(input, "f");
+    });
+
+    it('should NOT fire onSearch when typing separator characters', () => {
+        props = {
+            onSearch: function() {
+                expect(false).toBe(true);
+            },
+            separator: ", ",
+            valueField: "text"
+        };
+        render("Belarus");
+
+        const input = root.find("input")[0];
+        type(input, input.value + ",");
+    });
+
+    it('should autofill when typing', () => {
+        props = { suggest: true, valueField: "text", onSearch: render };
+        render("");
+
+        const input = root.find("input")[0];
+
+        type(input, "f");
+        expect(input.value).toEqual("Finland");
+    });
+
     //fails due to a bug! works only if a separator is provided
     it('should assert selection of suggested part of the word', () => {
         props = { valueField: "text", onSearch: render, suggest: true };
@@ -114,7 +105,7 @@ describe('KendoAutoComplete', withRoot(root => {
         expect(input.value.substring(input.selectionStart, input.selectionEnd)).toEqual("elarus");
     });
 
-    it('should delete suggested text selection on backspace', () => {
+    it('should NOT suggest when deleting', () => {
         props = { valueField: "text", onSearch: render, suggest: true, separator: ", " };
         render("");
 
