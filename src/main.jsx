d397f376a4c5b662f546b94b9eda50dda6b239b5
Alexander Valchev <a.valchevbox@gmail.com>
2016-05-04 11:23:09 +0300

    chore: remove AutoComplete component
    AutoComplete source, e2e, tests and docs are moved in the 'autocomplete'
    brach

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import * as Stateless from './stateless/main';
-import AutoComplete from './AutoComplete';
 import ComboBox from './ComboBox';
 import DropDownList from './DropDownList';
 
-module.exports = { AutoComplete, ComboBox, DropDownList, Stateless };
-
+module.exports = { ComboBox, DropDownList, Stateless };
